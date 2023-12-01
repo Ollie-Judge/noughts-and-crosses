9e30a44f9c0b7f2d76ce323fa4d9ae5ea8a426e7
Ollie-Judge <ollie.judge@hotmail.com>
2023-12-01 18:09:12 +0000

    added all win options

--- script.js
+++ script.js
@@ -8,7 +8,56 @@ let check = () => {
     userChoice.includes("div2") &&
     userChoice.includes("div3")
   ) {
-    console.log("win");
+    console.log("win1");
+  }
+  if (
+    userChoice.includes("div4") &&
+    userChoice.includes("div5") &&
+    userChoice.includes("div6")
+  ) {
+    console.log("win2");
+  }
+  if (
+    userChoice.includes("div7") &&
+    userChoice.includes("div8") &&
+    userChoice.includes("div9")
+  ) {
+    console.log("win3");
+  }
+  if (
+    userChoice.includes("div1") &&
+    userChoice.includes("div4") &&
+    userChoice.includes("div7")
+  ) {
+    console.log("win4");
+  }
+  if (
+    userChoice.includes("div2") &&
+    userChoice.includes("div5") &&
+    userChoice.includes("div8")
+  ) {
+    console.log("win5");
+  }
+  if (
+    userChoice.includes("div3") &&
+    userChoice.includes("div6") &&
+    userChoice.includes("div9")
+  ) {
+    console.log("win6");
+  }
+  if (
+    userChoice.includes("div1") &&
+    userChoice.includes("div5") &&
+    userChoice.includes("div9")
+  ) {
+    console.log("win7");
+  }
+  if (
+    userChoice.includes("div7") &&
+    userChoice.includes("div5") &&
+    userChoice.includes("div3")
+  ) {
+    console.log("win8");
   }
 };
 
