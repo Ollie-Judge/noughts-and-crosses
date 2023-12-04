342b5dd9cd509ae0fc720b7db9dd52058159ea03
Ollie-Judge <ollie.judge@hotmail.com>
2023-12-04 16:14:43 +0000

    added code to stop the computer choosing  user div

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
 let userChoice = [];
 
+let computerChoiceArray = [];
+
+const generateComputerChoice = () => {
+  let computerChoice = Math.floor(Math.random() * 9 + 1);
+  computerChoiceArray.push("div" + computerChoice);
+  console.log(computerChoiceArray);
+  outputComputerChoice();
+};
+
+const outputComputerChoice = () => {
+  if (userChoice.includes(computerChoiceArray.at(-1))) {
+    generateComputerChoice();
+  }
+  // get the last item in the computer choice array, everytime the user clicks a div
+  console.log("computer output" + computerChoiceArray.at(-1));
+  let selectedDiv = document.getElementById(computerChoiceArray.at(-1));
+  console.log("next step " + selectedDiv);
+  selectedDiv.innerHTML = "O";
+  selectedDiv.removeEventListener("click");
+
+  // access the div that the computer has chosen, then remove the event listener for the click and put a 'O' as the inner html
+};
+
 let gameBoardArray = document.querySelectorAll(".gameBoarditem");
 
 let check = () => {
@@ -8,56 +31,56 @@ let check = () => {
     userChoice.includes("div2") &&
     userChoice.includes("div3")
   ) {
-    console.log("win1");
+    window.alert("win1");
   }
   if (
     userChoice.includes("div4") &&
     userChoice.includes("div5") &&
     userChoice.includes("div6")
   ) {
-    console.log("win2");
+    window.alert("win2");
   }
   if (
     userChoice.includes("div7") &&
     userChoice.includes("div8") &&
     userChoice.includes("div9")
   ) {
-    console.log("win3");
+    window.alert("win3");
   }
   if (
     userChoice.includes("div1") &&
     userChoice.includes("div4") &&
     userChoice.includes("div7")
   ) {
-    console.log("win4");
+    window.alert("win4");
   }
   if (
     userChoice.includes("div2") &&
     userChoice.includes("div5") &&
     userChoice.includes("div8")
   ) {
-    console.log("win5");
+    window.alert("win5");
   }
   if (
     userChoice.includes("div3") &&
     userChoice.includes("div6") &&
     userChoice.includes("div9")
   ) {
-    console.log("win6");
+    window.alert("win6");
   }
   if (
     userChoice.includes("div1") &&
     userChoice.includes("div5") &&
     userChoice.includes("div9")
   ) {
-    console.log("win7");
+    window.alert("win7");
   }
   if (
     userChoice.includes("div7") &&
     userChoice.includes("div5") &&
     userChoice.includes("div3")
   ) {
-    console.log("win8");
+    window.alert("win8");
   }
 };
 
@@ -68,9 +91,10 @@ let userChosenDiv = gameBoardArray.forEach(function (div) {
     if (click === 0) {
       click = 1;
       console.log(click);
-      div.innerHTML = "Hello World";
+      div.innerHTML = "X";
       let chosenDivId = div.getAttribute("id");
       userChoice.push(chosenDivId);
+      generateComputerChoice();
       console.log(userChoice);
       console.log("click", chosenDivId);
     } else {
